4b236bc55a83c875fbbbb29c7be1d020219dd083
Stephan <stephan.watermeyer@dieboldnixdorf.com>
2017-02-03 13:54:07 +0100

    Cleanup

--- DailyBuild/ant-1.7.1/bin/pce.cmd
+++
@@ -1,2 +0,0 @@
-echo %1 %0
-ant24 -Dbasedir=%CD%   -f %~dp0/../../pceant/pce.xml %1 %2 %3
--- DailyBuild/ant-1.7.1/bin/pce.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-$(dirname $0)/ant -Dbasedir=`pwd`   -f $(dirname $0)/../../pceant/pce.xml $1 $2 $3
